b861890dad9b26c0b72d0561473858f56cb81d6e
FatemeAfshar20 <uttab4760@gmail.com>
2021-02-26 06:21:22 -0800

    Converting Splash Screen page to motion layout and adding some animation in Splash Screen page

--- app/src/main/java/com/sesootweb/sesootwebapp/view/activity/SplashActivity.java
+++ app/src/main/java/com/sesootweb/sesootwebapp/view/activity/SplashActivity.java
@@ -18,15 +18,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding= DataBindingUtil.setContentView(this,R.layout.splash_page);
 
-        mBinding.imvLogo.setAnimation(AnimationUtils.loadAnimation(this,R.anim.from_top_corner));
-        mBinding.tvTitle.setAnimation(AnimationUtils.loadAnimation(this,R.anim.from_right_to_left));
-        mBinding.tvUrl.setAnimation(AnimationUtils.loadAnimation(this,R.anim.from_left_to_right));
+        mBinding.tvTitle.setAnimation(AnimationUtils.loadAnimation(this,R.anim.fade_in));
+        mBinding.tvUrl.setAnimation(AnimationUtils.loadAnimation(this,R.anim.fade_in));
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 MainActivity.start(SplashActivity.this);
-
-                SplashActivity.this.finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         },4000);
 
